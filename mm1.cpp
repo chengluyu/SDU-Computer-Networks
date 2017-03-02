58e6f475c0ed4388f229489d34fbbd21d611dc6e
Cheng Luyu <chengluyu@live.cn>
2017-03-02 22:19:54 +0800

    The M/M/1 simulation implemented by C++

---
+++ mm1.cpp
@@ -0,0 +1,135 @@
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <random>
+#include <string>
+#include <vector>
+
+enum Constant {
+    kTotalPacketCount = 1000000,
+    kCheckPoint = 100000
+};
+
+struct Packet {
+    double arriveTime, serveTime;
+};
+
+class ExponentialGenerator {
+    std::random_device rd;
+    std::mt19937 gen;
+    std::exponential_distribution<> dist;
+public:
+    ExponentialGenerator(double lambda) : rd(), gen(rd()), dist(lambda) { }
+    
+    inline double next() {
+        return dist(gen);
+    }
+};
+
+class Queue {
+    std::queue<Packet> queue_;
+    
+    ExponentialGenerator packetServeTimeGen_, arriveTimeGen_;
+    double nextArriveTime_;
+    
+    // Create a new packet
+    inline Packet generatePacket(double arriveTime) {
+        return Packet { arriveTime, packetServeTimeGen_.next() };
+    }
+    
+public:
+    // Initialize the simulation queue with certain arguments
+    Queue() : packetServeTimeGen_(0.65), arriveTimeGen_(0.5), nextArriveTime_(0.0) { }
+    
+    void emit(double time) {
+        queue_.push(generatePacket(time));
+        nextArriveTime_ = time + arriveTimeGen_.next();
+    }
+    
+    // Returns how many packets in the queue
+    inline int size() const {
+        return static_cast<int>(queue_.size());
+    }
+    
+    inline double getNextArriveTime() {
+        return nextArriveTime_;
+    }
+    
+    // Returns whether there is any packet in the queue
+    inline bool hasNextPacket() const {
+        return !queue_.empty();
+    }
+    
+    Packet getNextPacket() {
+        Packet retValue = queue_.front();
+        queue_.pop();
+        return retValue;
+    }
+};
+
+class Logger {
+    std::vector<double> waitingTimeSamples_;
+    std::vector<int> queueLengthSamples_;
+public:
+    Logger() = default;
+    
+    inline void logWaitingTime(double time) {
+        waitingTimeSamples_.push_back(time);
+    }
+    
+    inline void logQueueLength(int length) {
+        queueLengthSamples_.push_back(length);
+    }
+    
+    void dumpToFile() {
+        std::ios::sync_with_stdio(false);
+
+        std::ofstream waitingTimeWriter { "waiting-time.txt" };
+        for (double val : waitingTimeSamples_) {
+            waitingTimeWriter << val << '\n';
+        }
+        waitingTimeWriter.close();
+        
+        std::ofstream queueLengthWriter { "queue-length.txt" };
+        for (int val : queueLengthSamples_) {
+            waitingTimeWriter << val << '\n';
+        }
+        queueLengthWriter.close();
+    }
+};
+
+
+int main() {
+    Logger logger;
+    
+    double currentTime = 0.0, currentPacketFinishTime = 0.0;
+    int simulatedPacketCount = 0;
+    
+    Queue queue;
+    
+    while (simulatedPacketCount < kTotalPacketCount) {
+        if (currentPacketFinishTime < queue.getNextArriveTime() && queue.hasNextPacket()) {
+            Packet packet = queue.getNextPacket();
+            simulatedPacketCount++;
+            if (simulatedPacketCount % kCheckPoint == 0) {
+                std::cout << "Simulated " << simulatedPacketCount << " packets.\n";
+            }
+            // Update time
+            currentTime = currentPacketFinishTime;
+
+            // Append to waiting time
+            logger.logWaitingTime(currentPacketFinishTime - packet.arriveTime);
+
+            currentPacketFinishTime = currentTime + packet.serveTime;
+        } else {
+            logger.logQueueLength(queue.size());
+            queue.emit(currentTime);
+            currentTime = queue.getNextArriveTime();
+        }
+    }
+    
+    std::cout << "Done.\n";
+
+    logger.dumpToFile();
+    return 0;
+}
