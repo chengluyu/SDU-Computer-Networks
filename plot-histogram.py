9926cfa76135206c514db22c479833b330c7fb72
Cheng Luyu <chengluyu@live.cn>
2017-03-04 15:00:26 +0800

    Plot with logarithmic axis

--- plot-histogram.py
+++ plot-histogram.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         data = map(float, f.readlines())
-        plt.hist(list(data), 100)
+        plt.hist(list(data), bins=200, log=True)
         plt.show()
     
