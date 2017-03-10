01a59cf97812f98b128d13efc82cb6f47ff05a4c
Cheng Luyu <chengluyu@live.cn>
2017-03-10 20:56:31 +0800

    Perfect M/M/1 result

--- mm1.py
+++ mm1.py
@@ -1,38 +1,67 @@
 from random import expovariate
 from queue import Queue
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class Packet:
-    def __init__(self, arrival_time):
+    def __init__(self, arrival_time, service_time):
         self.arrival_time = arrival_time
-        self.serving_time = expovariate(0.65)
+        self.service_time = service_time
 
-class Queue:
+
+class Generator:
     def __init__(self):
-        self.queue = Queue()
-    
-    def generate(self):
-        self.queue.
-        
+        self.next_arrival_time = expovariate(0.5)
+
+    def next(self):
+        pack = Packet(self.next_arrival_time, expovariate(0.65))
+        self.next_arrival_time += expovariate(0.5)
+        return pack
+
+
+def histogram(data):
+    counts, bins = np.histogram(data, bins=200, normed=True)
+    counts = [sum(counts[i:]) for i in range(len(counts))]
+    bins = (bins[:-1] + bins[1:]) / 2
+    plt.plot(bins, counts)
+    plt.semilogy()
+    plt.show()
 
 if __name__ == '__main__':
-    total_packet_count = 10000000
+    total_packet_count = 1000000
+    check_point_count = 100000
+
+    current_time = 0.0
+    current_packet_finished_time = 0.0
+    simulated_packet_count = 0
 
     queue = Queue()
-    current_time = 0.0
-    served_packet_count = 0
+    generator = Generator()
 
-    the_packet = None
+    waiting_time_samples = []
 
-    while served_packet_count < total_packet_count:
-        if the_packet is None:
+    while simulated_packet_count < total_packet_count:
+        # current packet will be served before next packet arrives
+        if current_packet_finished_time < generator.next_arrival_time:
+            simulated_packet_count += 1
+            if simulated_packet_count % check_point_count == 0:
+                print('Simulated %d packets' % simulated_packet_count)
+
+            # if queue is empty, pick next packet from generator
             if queue.empty():
-                current_time += expovariate(0.5)
-                the_packet = Packet(current_time)
+                packet = generator.next()
+                current_time = packet.arrival_time
+                current_packet_finished_time = packet.service_time + current_time
+                waiting_time_samples.append(0.0)
+            # if queue is not empty, pick next packet from queue
             else:
-                the_packet = queue.get()
+                packet = queue.get(block=False)
+                current_time = current_packet_finished_time
+                current_packet_finished_time = packet.service_time + current_time
+                waiting_time_samples.append(current_time - packet.arrival_time)
+        # next packet will arrive before current packet is finished
         else:
-            current_time += the_packet.serving_time
-            the_packet = None
-            
+            queue.put(generator.next())
 
-        
+    histogram(waiting_time_samples)
