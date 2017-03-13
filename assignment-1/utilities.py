79dbbf5aca6f6f12698a2018b41021b3f6f05879
Cheng Luyu <chengluyu@live.cn>
2017-03-13 19:38:23 +0800

    Arguments of 'Generator' can be customized

--- assignment-1/utilities.py
+++ assignment-1/utilities.py
@@ -10,12 +10,14 @@ class Packet:
 
 
 class Generator:
-    def __init__(self):
-        self.next_arrival_time = 0.0 + expovariate(0.5)
+    def __init__(self, arrival_lambda=0.3, service_lambda=1.4):
+        self.arrival_lambda = arrival_lambda
+        self.service_lambda = service_lambda
+        self.next_arrival_time = 0.0 + expovariate(arrival_lambda)
 
     def next(self):
-        pack = Packet(self.next_arrival_time, expovariate(0.65))
-        self.next_arrival_time += expovariate(0.5)
+        pack = Packet(self.next_arrival_time, expovariate(self.service_lambda))
+        self.next_arrival_time += expovariate(self.arrival_lambda)
         return pack
 
 
@@ -25,12 +27,15 @@ class Histogram:
 
     @staticmethod
     def plot(data):
-        counts, bins = np.histogram(data, bins=400, normed=True)
-        counts = [sum(counts[i:]) for i in range(len(counts))]
-        bins = (bins[:-1] + bins[1:]) / 2
-        plt.plot(bins, counts)
+        counts, bin_edges = np.histogram(data, bins=100, density=True)
+        cdf = [sum(counts[i:]) for i in range(len(counts))]
+        bins_avg = (bin_edges[:-1] + bin_edges[1:]) / 2
+        plt.plot(bins_avg, cdf)
 
     @staticmethod
     def show():
+        plt.title('Waiting Time Cumulative Distribution')
+        plt.xlabel('Time Epoch')
+        plt.ylabel('Probability')
         plt.semilogy()
         plt.show()
